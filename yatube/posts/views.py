b8f803a6e96b504fa68aee157ec5b48097acd9c5
Alexander Telepin <Telepin_Alexander@mail.ru>
2022-03-08 00:43:08 +0300

    Используем силу namespaсe

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'posts/index.html')
+    title = "Это главная страница проекта Yatube"
+    context = {
+        "title": title,
+    }
+    return render(request, "posts/index.html", context)
 
 
-def group_posts(request, slug):
-    return render(request, 'posts/group_list.html')
+def group_posts(request):
+    title = "Здесь будет информация о группах проекта Yatube"
+    context = {
+        "title": title,
+    }
+    return render(request, "posts/group_list.html", context)
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
-    path('', include('posts.urls')),
+    path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
 ]
