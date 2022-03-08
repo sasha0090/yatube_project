9794d3a1e10ac6629fdce0e7ea987f33b9a034be
Alexander Telepin <Telepin_Alexander@mail.ru>
2022-03-08 18:55:55 +0300

    Подготовка к сдаче

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -7,7 +7,7 @@ User = get_user_model()
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     description = models.TextField()
 
     def __str__(self):
@@ -20,7 +20,7 @@ class Post(models.Model):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name="posts"
     )
     group = models.ForeignKey(
         Group,
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,7 +3,7 @@ from .models import Post, Group
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.order_by("-pub_date")[:10]
 
     context = {
         "posts": posts,
@@ -13,10 +13,12 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    title = group.title
+    posts = Post.objects.filter(group=group).order_by("-pub_date")[:10]
 
     context = {
-        'group': group,
-        'posts': posts,
+        "title": title,
+        "group": group,
+        "posts": posts,
     }
     return render(request, "posts/group_list.html", context)
